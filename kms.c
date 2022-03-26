16c03cdf0d770fcbbe45cb4c83adb1ba4a31d5b5
Sarenur <saerua>
2022-03-26 07:48:16 -0700

    third commit

---
+++ kms.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#define KMS_PER_MILE 1.609
+#define print printf("hello")
+
+
+int main(){
+	double milesPerKm;
+	float a;
+	int b;
+	char c;
+	double kms;
+
+	printf("Enter the distance in miles:");
+	scanf("%lf", &miles);
+	printf("hello");
+
+	kms = KMS_PER_MILE * miles;
+
+	printf("Your miles value %f \t that equals %f kilometers.\n", miles, kms);
+	
+	return 0;
+}
